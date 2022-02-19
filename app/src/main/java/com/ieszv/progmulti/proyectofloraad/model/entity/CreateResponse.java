9b891476db6fb4503937792bde7494c39eab3ffb
Antonio José García Arias <73525861+ajgarciarias10@users.noreply.github.com>
2022-02-19 23:46:11 +0100

    --

---
+++ app/src/main/java/com/ieszv/progmulti/proyectofloraad/model/entity/CreateResponse.java
@@ -0,0 +1,16 @@
+package com.ieszv.progmulti.proyectofloraad.model.entity;
+
+public class CreateResponse {
+
+    public long id;
+
+    public CreateResponse() {
+    }
+
+    @Override
+    public String toString() {
+        return "CreateResponse{" +
+                "id=" + id +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/ieszv/progmulti/proyectofloraad/model/entity/RowsResponse.java
@@ -0,0 +1,16 @@
+package com.ieszv.progmulti.proyectofloraad.model.entity;
+
+public class RowsResponse {
+
+    public long rows;
+
+    public RowsResponse() {
+    }
+
+    @Override
+    public String toString() {
+        return "EditResponse{" +
+                "rows=" + rows +
+                '}';
+    }
+}
