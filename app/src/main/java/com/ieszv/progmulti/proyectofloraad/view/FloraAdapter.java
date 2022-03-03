a10823efd2cd62aa3e63728342effe18c697af91
ajgarciarias10 <ajgarciarias@gmail.com>
2022-03-03 14:42:15 +0100

    --FIN IDS QUEDA IMAGNE ACABAR

--- app/src/main/java/com/ieszv/progmulti/proyectofloraad/view/FloraAdapter.java
+++ app/src/main/java/com/ieszv/progmulti/proyectofloraad/view/FloraAdapter.java
@@ -10,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.ieszv.progmulti.proyectofloraad.R;
 import com.ieszv.progmulti.proyectofloraad.model.entity.Flora;
 import com.ieszv.progmulti.proyectofloraad.view.viewholder.ViewHolderFlora;
@@ -43,7 +44,7 @@ public class FloraAdapter extends RecyclerView.Adapter<ViewHolderFlora> {
         holder.tv_flora.setText(flora.getBiotipo());
         holder.tv_nombre.setText(flora.getNombre());
         //Picasso.get().load(url).memoryPolicy(MemoryPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_CACHE).into(holder.iv_Flora);
-        Glide.with(context).load(url).into(holder.iv_Flora);
+        Glide.with(context).load(url).diskCacheStrategy(DiskCacheStrategy.NONE).into(holder.iv_Flora);
 
         Log.v("xyz",url);
 
